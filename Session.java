13df50c2819fa26361c3a0b9e9c4f73e9e94657c
den1kurniawan <denikurniawan10085@gmail.com>
2025-07-15 11:21:36 +0800

    Add files via upload

---
+++ Session.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simapel;
+
+/**
+ *
+ * @author acer
+ */
+public class Session {
+    private static String nik;
+    private static String nama;
+
+    public static String get_nik() { return nik; }
+    public static void set_nik(String n) { nik = n; }
+
+    public static String get_nama() { return nama; }
+    public static void set_nama(String n) { nama = n; }
+}
+
